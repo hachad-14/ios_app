4664172a4624e26bff9800942e0a03ecc73b5bee
hachad-14 <Nasa132456>
2022-04-11 03:24:07 +0000

    test

--- lib/location.dart
+++ lib/location.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
+import 'api/location_api.dart';
+
 class locationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,114 @@
-// ignore_for_file: prefer_const_constructors
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-import 'location.dart';
-import 'local_auth_api.dart';
+void main() {
+  runApp(MyApp());
+}
 
-void main() => runApp(MaterialApp(
-  title: "HomePage",
-  home: MyApp(),
-  debugShowCheckedModeBanner: false,
-  )
-);
+class MyApp extends StatefulWidget {
+  // location
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var locationMessage = '';
+  String latitude ="";
+  String longitude ="";
+
+  // function for getting the current location
+  // but before that you need to add this permission!
+  void getCurrentLocation() async {
+    var position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    var lat = position.latitude;
+    var long = position.longitude;
+
+    // passing this to latitude and longitude strings
+    latitude = "$lat";
+    longitude = "$long";
+
+    setState(() {
+      locationMessage = "Latitude: $lat and Longitude: $long";
+    });
+  }
+
+  // function for opening it in google maps
+
+  void googleMap() async {
+    String googleUrl =
+        "https://www.google.com/maps/search/?api=1&query=$latitude,$longitude";
+
+    if (await canLaunch(googleUrl)) {
+      await launch(googleUrl);
+    } else
+      throw ("Couldn't open google maps");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'User location application',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: Scaffold(
-        backgroundColor: Colors.grey[600],
-        body: ListView(children: <Widget>[
-          Align(
-            alignment: Alignment(0, 0),
-            child: Container(
-              margin: const EdgeInsets.only(top:30.0),
-              height: 60,
-              width: 380,
-              decoration:
-              BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20),
-              boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.3),spreadRadius: 0.5,blurRadius: 2, offset: Offset(0, 1), )]
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.location_on,
+                size: 45.0,
+                color: Colors.white,
               ),
-              child: Center(
-                child: Text('Project b0.1',style: TextStyle(fontSize: 20),),
+              SizedBox(
+                height: 20.0,
               ),
-            ),
+              Text(
+                "Get User Location",
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                locationMessage,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 05.0,
+              ),
+
+              // button for taking the location
+              FlatButton(
+                color: Colors.white,
+                onPressed: () {
+                  getCurrentLocation();
+                },
+                child: Text("Get User Location"),
+              ),
+            
+              FlatButton(
+                color: Colors.white,
+                onPressed: () {
+                  googleMap();
+                },
+                child: Text("Open GoogleMap"),
+              ),
+            ],
           ),
-          Align(
-            //location//
-            alignment: Alignment(-0.9, 0),
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,              
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top:50),
-                    height: 180,
-                    width: 180,
-                    decoration:
-                    BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20),
-                      boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.3),spreadRadius: 0.5,blurRadius: 2, offset: Offset(0, 1), )]
-                    ),
-                    child: InkWell(
-                      onTap: () async {
-                        final isAuthentificated = await LocalAuthApi.authenticate();
-                        if (isAuthentificated) {
-                          Navigator.push(context,
-                            CupertinoPageRoute(builder:(context) => locationScreen()),
-                          );
-                        }
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,    
-                        children: [
-                          Text("Location", style: TextStyle(fontSize: 20)),
-                          Padding(
-                            padding: EdgeInsets.only(top: 0),
-                            child: Image.asset('assets/images/location.png',fit: BoxFit.cover,height: 100,),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Container(
-                      child: Text("Test", style: TextStyle(),textAlign: TextAlign.center,),
-                      margin: const EdgeInsets.only(top:50),
-                      height: 180,
-                      width: 180,
-                      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20),
-                      boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.3),spreadRadius: 0.5,blurRadius: 2, offset: Offset(0, 1))]
-                    ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-           Align(
-            alignment: Alignment(-0.9, 0),
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,              
-                children: [
-                  Container(
-                    child: Text("Test", style: TextStyle(),textAlign: TextAlign.center,),
-                    margin: const EdgeInsets.only(top:20),
-                    height: 180,
-                    width: 180,
-                    decoration:
-                    BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20),
-                    boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.3),spreadRadius: 0.5,blurRadius: 2, offset: Offset(0, 1), )]
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Container(
-                      child: Text("Test", style: TextStyle(),textAlign: TextAlign.center,),
-                      margin: const EdgeInsets.only(top:20),
-                      height: 180,
-                      width: 180,
-                      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20),
-                      boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.3),spreadRadius: 0.5,blurRadius: 2, offset: Offset(0, 1))]
-                    ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ]),
+        ),
       ),
     );
   }
